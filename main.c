b592f675aaa925ae7f1050dcf61d368cee7239bc
lucas paes silva <lucaspaessilva183@gmail.com>
2026-06-16 22:33:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -22,8 +22,8 @@ int main() {
     // int resultado_multiplicacao = multiplicar(7, 6);
     // printf("Multiplicacao (7 * 6): %d\n", resultado_multiplicacao);
 
-    // float resultado_divisao = dividir(100.0, 4.0);
-    // printf("Divisao (100.0 / 4.0): %.2f\n", resultado_divisao);
+    float resultado_divisao = dividir(100.0, 4.0);
+    printf("Divisao (100.0 / 4.0): %.2f\n", resultado_divisao);
 
     // float resultado_divisao_erro = dividir(10.0, 0.0); // Teste de erro
     // printf("Divisao (10.0 / 0.0): %.2f\n", resultado_divisao_erro); // Deve indicar erro
